08624f6cad997b4ca2ed97c3ab79990d9acb4f52
Saif Ur Rehman <saif@paralogic.io>
2023-11-28 21:31:17 +0500

    refactor(Territory): move erpnext code outside of crm app

--- crm/crm/doctype/territory/territory.py
+++ crm/crm/doctype/territory/territory.py
@@ -2,8 +2,6 @@
 # License: GNU General Public License v3. See license.txt
 
 import frappe
-from frappe import _
-from frappe.utils import flt
 from frappe.utils.nestedset import NestedSet, get_root_of
 
 
@@ -13,11 +11,6 @@ class Territory(NestedSet):
 	def validate(self):
 		if not self.parent_territory:
 			self.parent_territory = get_root_of("Territory")
-
-		for d in self.get('targets') or []:
-			if not flt(d.target_qty) and not flt(d.target_alt_uom_qty) and not flt(d.target_amount):
-				frappe.throw(_("Row {0}: Either Target Stock Qty or Target Contents Qty or Target Amount is mandatory.")
-					.format(d.idx))
 
 	def on_update(self):
 		super(Territory, self).on_update()
