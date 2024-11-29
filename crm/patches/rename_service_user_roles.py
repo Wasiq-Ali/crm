d515c7246155ac9ba0030c04162e5d893913cf4b
Saif Ur Rehman <saif@paralogic.io>
2024-11-29 15:10:43 +0500

    chore: Rename Projects User to Service User

---
+++ crm/patches/rename_service_user_roles.py
@@ -0,0 +1,11 @@
+import frappe
+
+
+def execute():
+	rename_map = {
+		"Projects User": "Service User",
+		"Projects User (Read Only)": "Service User (Read Only)",
+	}
+	for old, new in rename_map.items():
+		if frappe.db.exists("Role", old):
+			frappe.rename_doc("Role", old, new, force=True)
